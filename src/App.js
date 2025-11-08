02117c3a325afa56e947018e4f3033fbbc369d4f
shimul <darkegale3536@gmail.com>
2025-11-08 14:56:37 +0600

    router issu

--- src/App.js
+++ src/App.js
@@ -8,33 +8,21 @@ import Project from "./Page/project";
 import Contact from "./Page/contact";
 import Notfound from "./Page/notfound";
 
-
 function App() {
-const navLinks =
-  process.env.NODE_ENV === "production"
-    ? [
-        { name: "Home", href: "#/" },
-        { name: "Services", href: "#/services" },
-        { name: "Projects", href: "#/project" },
-        { name: "Contact", href: "#/contact" },
-      ]
-    : [
-        { name: "Home", href: "/" },
-        { name: "Services", href: "/services" },
-        { name: "Projects", href: "/project" },
-        { name: "Contact", href: "/contact" },
-      ];
+  const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "Services", href: "/services" },
+    { name: "Projects", href: "/project" },
+    { name: "Contact", href: "/contact" },
+  ];
 
-
-  const Router =
-    process.env.NODE_ENV === "production" ? HashRouter : BrowserRouter;
+  const Router = process.env.NODE_ENV === "production" ? HashRouter : BrowserRouter;
 
   return (
     <Router>
       <NavbarEffect links={navLinks}/>
       <Routes>
-        <Route path="#/" element={<Home/>}/>
-        <Route path="/home" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/project" element={<Project/>}/>
         <Route path="/services" element={<Services/>}/>
         <Route path="/contact" element={<Contact/>}/>
@@ -45,3 +33,4 @@ const navLinks =
 }
 
 export default App;
+
