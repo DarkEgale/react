9566b2d9df55fdb1b5e5a308b94f31109fd439ee
shimul <darkegale3536@gmail.com>
2025-11-09 21:18:12 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import NavbarEffect from './Components/navbar';
@@ -7,26 +7,29 @@ import Services from "./Page/services";
 import Project from "./Page/project";
 import Contact from "./Page/contact";
 import Notfound from "./Page/notfound";
+import About from "./Page/about";
 
 function App() {
   const navLinks = [
-    { name: "Home", href: "/#/" },
-    { name: "Services", href: "/#/services" },
-    { name: "Projects", href: "/#/project" },
-    { name: "Contact", href: "/#/contact" },
+    { name: "Home", href: "#" },
+    { name: "About", href: "#about" },
+    { name: "Services", href: "#services" },
+    { name: "Projects", href: "#project" },
+    { name: "Contact", href: "#contact" },
   ];
 
   return (
-    <HashRouter>
+    <BrowserRouter>
       <NavbarEffect links={navLinks} />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="#" element={<Home />} />
+        <Route path="#about" element={<About/>}/>
+        <Route path="#project" element={<Project />} />
+        <Route path="#services" element={<Services />} />
+        <Route path="#contact" element={<Contact />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
--- src/Page/contact.jsx
+++ src/Page/contact.jsx
@@ -1,5 +1,25 @@
+
+import '../styles/contact.css'
+
 export default function Contact(){
     return(
-        <h1>This is Contact page</h1>
+        <section id='contact'>
+            <form action="">
+                Name: <br />
+                <input type="text" name="name" id="name" /><br />
+                Email: <br />
+                <input type="email" name="email" id="email" /> <br />
+                Massage: <br />
+                <textarea name="massage" id="massage"></textarea> <br />
+                <input type="submit" value="Submit" />
+            </form>
+            <div id="social">
+                <button>Facebook</button>
+                <button>Facebook</button>
+                <button>Facebook</button>
+                <button>Facebook</button>
+                <button>Facebook</button>
+            </div>
+        </section>
     )
 }
